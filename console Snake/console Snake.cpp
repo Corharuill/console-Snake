ff76510750373776351094bafd35c0298647c026
Lenin-Grib <dessotterro@mail.ru>
2024-06-30 14:42:02 +0300

    Fixe getRandPosition()

--- console Snake/console Snake.cpp
+++ console Snake/console Snake.cpp
@@ -16,12 +16,12 @@ struct Position
 struct Variables
 {
 	bool     game_over;
-	const    int WIDTH  = 20;
-	const    int HEIGHT = 20;
-	int      score      = 0;
-	bool     hungary    = false;
-	Position pos_snake;
-	Position old_pos_food;
+	const    int WIDTH    = 20;
+	const    int HEIGHT   = 20;
+	int      score        = 0;
+	bool     hungary      = false;
+	Position pos_snake    = { 1, 1 };
+	Position old_pos_food = { 1, 1 };
 	Position new_pos_food;
 
 	enum  Direction
@@ -46,31 +46,25 @@ void gotoxy(int x, int y)
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
+// Возвращает рандомно заданные координаты для еды
+// TO DO исправить чтобы возвращал координаты для любого объекта
 Position getRandPosition()
 {
 	srand(time(0));
-
-	if (peremen.old_pos_food.x != peremen.HEIGHT && peremen.old_pos_food.y != peremen.WIDTH
-		&& peremen.old_pos_food.x != 0 && peremen.old_pos_food.y != 0)
-	{
-		peremen.old_pos_food.x = rand() % peremen.WIDTH;
-		peremen.old_pos_food.y = rand() % peremen.HEIGHT;
-	}
+	peremen.old_pos_food.x = (rand() % (peremen.WIDTH  -1)) + 1;
+	peremen.old_pos_food.y = (rand() % (peremen.HEIGHT -1)) + 1;
 	return peremen.old_pos_food;
-   
 }
 
 // Стартовые данные
 void Setup()
 {
 	// Стартовыая позиция еды
-
 	peremen.old_pos_food = getRandPosition();
-	// Стартовая позиция персонажа
 
+	// Стартовая позиция персонажа
 	peremen.pos_snake.x = rand() % peremen.WIDTH;
 	peremen.pos_snake.y = rand() % peremen.HEIGHT;
-
 }
 
 // Метод отрисовки
@@ -78,7 +72,6 @@ void Draw()
 {
 	for (size_t y = 0; y <= peremen.HEIGHT ; y++)
 	{
-
 		for (size_t x = 0; x <= peremen.WIDTH; x++)
 		{
 			// отрисовка краев карты
@@ -162,8 +155,6 @@ void Logic()
 // Главный метод
 int main()
 {
-	
-
 	Setup();
 	while (true)
 	{
@@ -178,6 +169,5 @@ int main()
 			Logic();
 		}
 	}
-	
 	return 0;
 }
